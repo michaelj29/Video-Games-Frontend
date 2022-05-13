95c9050886f4d13d9bb9d1f92d7174dc9553e257
Johnny Lombardi <jlombardi004@gmail.com>
2022-05-13 11:45:23 -0700

    set platform data + have a 2nd array coming in with the count just need to display

--- video-games/src/Components/VideoGameChart.jsx
+++ video-games/src/Components/VideoGameChart.jsx
@@ -1,31 +1,45 @@
-import { React, } from "react";
+import { React } from "react";
 import { Chart } from "react-google-charts";
 
-
-const VideoGameChart = ({videoGames}) => {
-
-
+const VideoGameChart = ({ videoGames }) => {
   function generateDateFormChart() {
-
-    console.log("These Video Games", videoGames)
-    let filteredGames = videoGames.filter(game => game.year > 2013);
-
-
-
+    // console.log("These Video Games", videoGames);
+    let filteredGames = videoGames.filter(game => game.year >= 2013);
+    // console.log("filtered games", filteredGames);
+    let platforms = filteredGames.map(game => {
+      return game.platform;
+    });
+    console.log(platforms, "platforms");
+    let distinctPlatforms = [...new Set(platforms)];
+    // console.log("Distinct Platforms", distinctPlatforms);
+    let platformArrays = distinctPlatforms.map(platform => {
+      console.log("2nd platform array", allGamesPlatform);
+      console.log("hope!!", distinctGlobalSales);
+      return [platform, allGamesPlatform, "silver"];
+    });
+    // console.log("platform array", platformArrays);
     const data = [
       ["Platform", "Sales", { role: "style" }],
-      ["Playstation", 8.94, "red"], // RGB value
-      ["Silver", 10.49, "red"], // English color name
-      ["Gold", 19.3, "red"],
-      ["Platinum", 21.45, "red"], // CSS-style declaration
+      ...platformArrays,
+      // ["Playstation", 8.94, "red"], // RGB value
+      // ["Silver", 10.49, "red"], // English color name
+      // ["Gold", 19.3, "red"],
+      // ["Platinum", 21.45, "red"], // CSS-style declaration
     ];
-    
+    console.log("data", data);
+
     return data;
   }
 
   return (
     <>
-      <Chart chartType="ColumnChart" width="100%" height="400px" data={generateDateFormChart} />;
+      <Chart
+        chartType="ColumnChart"
+        width="100%"
+        height="400px"
+        data={generateDateFormChart()}
+      />
+      ;
     </>
   );
 };
