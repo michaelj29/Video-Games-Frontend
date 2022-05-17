57fa9d86070fbfe36a532d4a4e3e63ea0e7e69f6
michaelj29 <johnson_michael1329@yahoo.com>
2022-05-17 10:00:03 -0500

    Remove comments from VideoGameChart file.

--- video-games/src/Components/VideoGameChart.jsx
+++ video-games/src/Components/VideoGameChart.jsx
@@ -5,41 +5,28 @@ const VideoGameChart = ({ videoGames }) => {
 
 
   function generateDateFormChart() {
-    // console.log("These Video Games", videoGames);
+
     let filteredGames = videoGames.filter(game => game.year >= 2013);
-    // console.log("filtered games", filteredGames);
     let platforms = filteredGames.map(game => {
       return game.platform;
     });
-    // console.log(platforms, "platforms");
     let distinctPlatforms = [...new Set(platforms)];
-    //console.log("Distinct Platforms", distinctPlatforms);
     let platformArrays = distinctPlatforms.map(platform => {
       let sum = 0;
       let allGamesForPlatform = filteredGames.filter(
         game => game.platform === platform
       );
       for (let i = 0; i < allGamesForPlatform.length; i++){
-        //NEED GLOBAL SALES FOR EACH PLATFORM 
-        //THE LOGIC NEEDS TO BE COMPLETED 
-        //console.log(allGamesForPlatform[i].globalSales + " " + allGamesForPlatform[i] + " " + allGamesForPlatform[i].platform)
         console.log(allGamesForPlatform[i].globalSales)
           sum += allGamesForPlatform[i].globalSales;
         }
         return [platform, sum, "red"];
   });
 
-    // console.log("platform array", platformArrays);
     const data = [
       ["Platform", "Sales", { role: "style" }],
       ...platformArrays,
-      // ["Playstation", 8.94, "red"], // RGB value
-      // ["Silver", 10.49, "red"], // English color name
-      // ["Gold", 19.3, "red"],
-      // ["Platinum", 21.45, "red"], // CSS-style declaration
     ];
-    // console.log("data", data);
-
     return data;
   }
 
